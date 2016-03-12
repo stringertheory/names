338a33448e6fc15c0143c6dd49a48a60017dbd21
stringertheory <mike.stringer@datascopeanalytics.com>
2016-03-11 23:32:12 -0600

    added skeleton web app

---
+++ web/main.py
@@ -0,0 +1,51 @@
+import sys
+import os
+
+from flask import Flask, render_template, request, send_from_directory
+from flask_frozen import Freezer
+from flask.ext.assets import Environment, Bundle
+from flask.ext.pymongo import PyMongo
+from webassets.filter import get_filter
+
+app = Flask(__name__)
+app.config.from_object('settings')
+freezer = Freezer(app)
+assets = Environment(app)
+mongo = PyMongo(app)
+
+assets.register('js_all', Bundle(
+    'js/main.js',
+    'js/another.js',
+    filters=('uglifyjs',),
+    output='build/main.%(version)s.js',
+    depends='js/*.js',
+))
+
+scss_path = os.path.join(app.static_folder, 'scss')
+scss_filter = get_filter('scss', as_output=False, load_paths=[scss_path])
+assets.register('css_all', Bundle(
+    'scss/styles.scss',
+    filters=(scss_filter,),
+    output='build/main.%(version)s.css',
+    depends='scss/*.scss',
+))
+
+@app.route('/')
+def index():
+    poems = mongo.db.poems.find(None, {'_id': True, 'title': True})
+    return render_template('index.html', poems=poems)
+
+@app.route('/poem/<id>')
+def poem(id):
+    poem = mongo.db.poems.find_one({'_id': id})
+    return render_template('poem.html', poem=poem)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'img/favicon.ico')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
---
+++ web/settings.py
@@ -0,0 +1,6 @@
+DEBUG = True
+UGLIFYJS_EXTRA_ARGS = ['-m', '-c']
+SASS_STYLE = 'compressed'
+ASSETS_DEBUG = False
+MONGO_DBNAME = 'poetry'
+GOOGLE_ANALYTICS = '' # can put identifier
